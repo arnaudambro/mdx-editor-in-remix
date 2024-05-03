83c02f0cf3c578b991a665fb964416e251a44555
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-05-03 10:33:54 +0200

    ferat: add all plugins

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { ClientOnly } from "remix-utils/client-only";
-import { MDXEditor } from "~/components/editor.client";
-import type { V2_MetaFunction } from "@remix-run/node";
+import MDXFullEditor from "~/components/MDXFullEditor";
+import type { MetaFunction } from "@remix-run/node";
 
-export const meta: V2_MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
 };
 
 export default function Index() {
   return (
     <ClientOnly fallback={<p>Loading...</p>}>
-      {() => <MDXEditor markdown="Hello world" />}
+      {() => <MDXFullEditor markdown="Hello world" />}
     </ClientOnly>
   );
 }
